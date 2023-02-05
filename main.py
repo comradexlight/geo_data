bf9d5972b0a3b22115c0ed775a8b9a16193f8303
comradexlight <comradexlight@gmail.com>
2023-02-05 10:45:39 +0300

    fixed mistake with saving map.html template, added doc strings

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+'''The simple server for drawing plot boundaries by cadastre number'''
 from fastapi import FastAPI, Form
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from utils import draw_map
 
 
 app = FastAPI()
- 
-@app.get("/")
-def root() -> FileResponse:
-    return FileResponse("templates/index.html")
- 
- 
-@app.post("/cadastre_number")
-def postdata(cadastre_number = Form()) -> FileResponse:
-     draw_map(cadastre_number)
-     return FileResponse(f"templates/map.html")
 
+@app.get("/")
+async def index() -> FileResponse:
+    '''This function gives index.html by get request'''
+    return FileResponse("templates/index.html")
+
+
+@app.post("/cadastre_number")
+async def post_cadastre_number(cadastre_number = Form()) -> HTMLResponse:
+    '''This fucntion gives the page with the map and the boundary'''
+    content = draw_map(cadastre_number)
+    return HTMLResponse(content=content, status_code=200)
--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
+'''The Module with the logic of plot boundaries on the map'''
 import folium
 from rosreestr2coord import Area
 
 
-def draw_map(cadastre_number: str) -> None:
+def draw_map(cadastre_number: str) -> str:
     '''Creates a page with a map and borders of the object by cadastrу number'''
     area = Area(cadastre_number, with_proxy=True)
     points = [coords_list[::-1] for coords_list in area.xy[0][0]]
-    m = folium.Map(location=(area.center["y"], area.center["x"]),
+    map_ = folium.Map(location=(area.center["y"], area.center["x"]),
                              tiles="OpenStreetMap",
                              zoom_start=20)
-    folium.Polygon(locations=points, color="yellow").add_to(m)
-    m.save(f"templates/map.html")
-
+    folium.Polygon(locations=points, color="yellow").add_to(map_)
+    return map_._repr_html_()
